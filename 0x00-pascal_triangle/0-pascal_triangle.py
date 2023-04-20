79cbb57dd3996a28254f964a6b4897e001581831
Fome-a <ofomearubs@gmail.com>
2023-04-20 12:56:30 +0100

    tasks

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""Module for working on creating Pascal's Triangle"""
-
+'''A module for working with Pascal's triangle.
+'''
 
 
 def pascal_triangle(n):
-    """Creates a list of lists of integers representingthe Pascal's triangle of a given integer."""
-    list1 = []
-    if type(n) is not int or n < 0:
-        return list1
-    for i in range (n):
-        inner_list = []
-        for j in range(i+1):
-            if j == 0 or j == i:
-                inner_list.append(1)
+    '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
+    '''
+    pt = []
+    if type(n) is not int or n <= 0:
+        return pt
+    for x in range(n):  # 1
+        ct = []
+        for y in range(x+1):
+            if y == 0 or y == x:
+                ct.append(1)
             else:
-                inner_list.append((list1[i-1][j-1])+ (list1[i-1][j]))
-        list1.append(inner_list)
-    return list1
+                ct.append(pt[x-1][y]+pt[x-1][y-1])
+        pt.append(ct)
+    return pt
